72683c4884151224c987a024198ada2281452f57
Substance-live <shpion160806@gmail.com>
2025-07-16 13:49:47 +0300

    fixes container

--- src/api/anime.py
+++ src/api/anime.py
@@ -4,9 +4,12 @@ from src.anime.schemas import AnimeSchema, AnimeCreateSchema
 from src.anime.service import AnimeService
 
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/anime",
+    tags=["Аниме"]
+)
 
-@router.get("/anime")
+@router.get("")
 async def get_anime(anime_id: int) -> AnimeSchema:
     try:
         ret = await AnimeService.get(anime_id)
@@ -14,32 +17,32 @@ async def get_anime(anime_id: int) -> AnimeSchema:
         raise HTTPException(404, "Wrong anime_id")
     return ret
 
-@router.get("/anime/all")
+@router.get("/all")
 async def get_all_anime(filter_by: dict | None = None) -> list[AnimeSchema]:
     ret = await AnimeService.list(filter_by)
     return ret
 
-@router.put("/anime")
+@router.put("")
 async def update_anime(anime_id: int, anime_model: AnimeCreateSchema):
     ret = await AnimeService.update(anime_id, anime_model)
     return {"ok": ret == 1}
 
-@router.delete("/anime")
+@router.delete("")
 async def delete_anime(anime_id: int):
     ret = await AnimeService.delete(anime_id)
     return {"ok": ret == 1}
 
-@router.delete("/anime/all")
+@router.delete("/all")
 async def delete_all_anime():
     ret = await AnimeService.delete_all()
     return {"ok": ret == 1}
 
-@router.get("/anime/count")
+@router.get("/count")
 async def get_count_anime() -> int:
     ret = await AnimeService.count()
     return ret
 
-@router.post("/anime")
+@router.post("")
 async def post_anime(anime_model: AnimeCreateSchema) -> int:
     ret = await AnimeService.add(anime_model)
     return ret.id
--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
+from contextlib import asynccontextmanager
+
 from src.api import main_router
+from src.db import create_tables, delete_tables
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_tables()
+    yield
+    await delete_tables()
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(main_router)
